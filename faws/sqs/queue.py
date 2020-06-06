05d985b7b2e9dd9269e853dfa43cd9c0eb960608
takeru911 <t.maehara911@gmail.com>
2020-06-06 15:10:20 +0900

    send_messageを実装、send_message自体のテストコードが未実装

--- faws/sqs/queue.py
+++ faws/sqs/queue.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 import datetime
+import uuid
+import dataclasses
+from typing import Dict, Optional
 
 
 class Queue:
     def __init__(self, queue_name: str):
         self._queue_name = queue_name
-        self._queue_url = f"https://sqs-{self.queue_name}"
+        self._queue_url = f"https://localhost:5000/queues/{self.queue_name}"
         self._created_at = datetime.datetime.now()
+        self._messages = {}
 
     @property
     def queue_name(self) -> str:
@@ -20,8 +24,51 @@ class Queue:
     def created_at(self) -> datetime:
         return self._created_at
 
+    @property
+    def messages(self):
+        return self._messages
+
+    def add_message(
+            self,
+            message_body: str,
+            message_attributes: Dict = None,
+            delay_seconds: int = 0,
+            message_system_attributes: Dict = None,
+            message_deduplication_id: str = None,
+            message_group_id: str = None
+    ) -> Message:
+        message = Message(
+            message_body,
+            message_attributes=message_attributes,
+            delay_seconds=delay_seconds,
+            message_system_attributes=message_system_attributes,
+            message_deduplication_id=message_deduplication_id,
+            message_group_id=message_group_id
+        )
+        self._messages[message.message_id] = message
+        return message
+
     def __eq__(self, other: Queue) -> bool:
         return self.queue_url == other.queue_url \
                and self.queue_name == other.queue_name \
                and self.created_at == other.created_at
 
+
+def generate_uuid() -> str:
+    return str(uuid.uuid4())
+
+
+@dataclasses.dataclass
+class Message:
+    message_body: str
+    message_id: str = generate_uuid()
+    message_attributes: Optional[Dict] = None
+    delay_seconds: int = 0
+    message_system_attributes: Optional[Dict] = None
+    message_deduplication_id: Optional[str] = None
+    message_group_id: Optional[str] = None
+    message_inserted_at: datetime = datetime.datetime.now()
+    message_deliverable_time: datetime = dataclasses.field(init=False)
+
+    def __post_init__(self):
+        self.message_deliverable_time = self.message_inserted_at + datetime.timedelta(seconds=self.delay_seconds)
--- tests/sqs/test_queue.py
+++ tests/sqs/test_queue.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from faws.sqs.queue import Queue
+from faws.sqs.queue import Queue, Message
 from unittest import mock
 
 
@@ -12,3 +12,22 @@ def test_equal():
         other = Queue("other_queue")
         assert queue == same
         assert queue != other
+
+
+def test_add_message():
+    queue = Queue("test-queue")
+    message = queue.add_message(
+        "takerun"
+    )
+    assert message.message_id in queue.messages
+
+
+def test_message_set_delay():
+    message = Message(
+        message_body="hoge",
+        message_id="1111",
+        message_inserted_at=datetime(2020, 5, 28, 0, 0, 0),
+        delay_seconds=10
+    )
+
+    assert message.message_deliverable_time == datetime(2020, 5, 28, 0, 0, 10)
