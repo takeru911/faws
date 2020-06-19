3c154ef2bcd5ae5a4841e636a33d69760b9eebcc
takeru911 <t.maehara911@gmail.com>
2020-06-19 20:43:10 +0900

    add_messageのtestをparameterizeに

--- tests/sqs/test_queue.py
+++ tests/sqs/test_queue.py
@@ -3,7 +3,6 @@ from faws.sqs.queue import Queue, Message
 from unittest import mock
 import pytest
 
-
 def test_equal():
     now = datetime(2020, 5, 28, 0, 0, 0)
     with mock.patch("datetime.datetime") as dt:
@@ -15,22 +14,21 @@ def test_equal():
         assert queue != other
 
 
-
-def test_add_message(dt):
-    dt.return_value = datetime(2020, 5, 20, 0, 0, 0)
-    queue = Queue("test-queue")
-    message = queue.add_message("takerun")
-    assert message.message_id in queue.messages
-
 @mock.patch("datetime.datetime")
-@pytest.mark.parametrize("message_attr,message", [
-    {"message_body": "test", "message_attributes": None, "delay_seconds": 0},
-    {"message_body": "test", "message_attributes": None, "delay_seconds": 10},
-])
+@pytest.mark.parametrize("delay_seconds", [0, 10])
+def test_add_message(dt, delay_seconds):
+    now = datetime(2020, 5, 20, 0, 0, 0)
+    expected_deliverable_time = datetime(2020, 5, 20, 0, 0, 0 + delay_seconds)
+    dt.now.return_value = now
+    queue = Queue("test-queue")
+    message = queue.add_message("takerun", delay_seconds=delay_seconds)
+    assert message.message_id in queue.messages and message.message_deliverable_time == expected_deliverable_time
+
+
 def test_get_message():
     queue = Queue("test-queue")
 
-    with mock.patch("uuid.uuid4", return_value="1111"), mock.patch("datetime.datetime."):
+    with mock.patch("uuid.uuid4", return_value="1111"):
         message = queue.add_message("takerun")
 
         assert queue.get_message() == message
