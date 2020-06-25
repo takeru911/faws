62f30d6dab8af52b6187435335c069428af170ba
takeru911 <t.maehara911@gmail.com>
2020-06-25 19:54:56 +0900

    SQSErrorの基底クラスを

--- faws/sqs/error.py
+++ faws/sqs/error.py
@@ -1,2 +1,8 @@
-class NonExistentQueue(Exception):
+class SQSError(Exception):
+    @property
+    def message(self):
+        raise NotImplementedError
+
+
+class NonExistentQueue(SQSError):
     message = "The specified queue does not exist for this wsdl version."
