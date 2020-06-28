a9bf120df5aa4b1177cd106476b6cdadcba77db5
takeru911 <t.maehara911@gmail.com>
2020-06-28 11:18:56 +0900

    例外創出チェク

--- tests/sqs/test_message.py
+++ tests/sqs/test_message.py
@@ -28,6 +28,22 @@ def test_from_request_data():
     assert actual == expected
 
 
+def test_from_request_data_invalid_type():
+    request_data = {
+        "MessageAttribute.1.Name": "City",
+        "MessageAttribute.1.Value.DataType": "Strong",
+        "MessageAttribute.1.Value.StringValue": "Any+City",
+        "MessageAttribute.2.Name": "Greeting",
+        "MessageAttribute.2.Value.DataType": "Binary",
+        "MessageAttribute.2.Value.BinaryValue": "SGVsbG8sIFdvcmxkIQ%3D%3D",
+        "MessageAttribute.3.Name": "Population",
+        "MessageAttribute.3.Value.DataType": "Number",
+        "MessageAttribute.3.Value.StringValue": "1250800",
+    }
+    with pytest.raises(ValueError):
+        MessageAttribute.from_request_data(request_data)
+
+
 @pytest.mark.parametrize(
     "message_attribute,expected",
     [
