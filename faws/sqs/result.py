159964e3ac2b54096676975e963514c6d57fd735
takeru911 <t.maehara911@gmail.com>
2020-06-28 10:53:16 +0900

    resultもっと共通化

---
+++ faws/sqs/result.py
@@ -0,0 +1,64 @@
+import dataclasses
+from dict2xml import dict2xml
+from typing import Dict, Optional
+from faws.sqs.error import SQSError
+from abc import abstractmethod
+
+
+class Result:
+
+    @property
+    def response_code(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def generate_response(self) -> str:
+        raise NotImplementedError
+
+
+@dataclasses.dataclass()
+class SuccessResult(Result):
+    operation_name: str
+    result_data: Optional[Dict]
+    request_id: str
+    response_code: int = 200
+
+    def generate_response(self) -> str:
+        if self.result_data is not None:
+            return dict2xml(
+                {
+                    f"{self.operation_name}Response": {
+                        f"{self.operation_name}Result": self.result_data,
+                        "ResponseMetadata": {"RequestId": self.request_id},
+                    }
+                }
+            )
+        return dict2xml(
+            {
+                f"{self.operation_name}Response": {
+                    "ResponseMetadata": {"RequestId": self.request_id},
+                }
+            }
+        )
+
+
+@dataclasses.dataclass()
+class ErrorResult(Result):
+    error: SQSError
+    request_id: str
+    response_code: int = 400
+
+    def generate_response(self) -> str:
+        return dict2xml(
+            {
+                f"ErrorResponse": {
+                    "Error": {
+                        "Type": "Sender",
+                        "Code": f"AWS.SimpleQueueService.{self.error.__class__.__name__}",
+                        "Message": self.error.message,
+                        "Detail": {},
+                    },
+                    "ResponseMetadata": {"RequestId": self.request_id},
+                },
+            }
+        )
---
+++ tests/sqs/test_result.py
@@ -0,0 +1,48 @@
+import pytest
+from dict2xml import dict2xml
+from faws.sqs.error import NonExistentQueue
+from faws.sqs.result import SuccessResult, ErrorResult
+
+
+@pytest.mark.parametrize(
+    "result,response",
+    [
+        (
+                SuccessResult(
+                    operation_name="test",
+                    result_data={"test_result": "hoge"},
+                    request_id="111",
+                ),
+                {
+                    "testResponse": {
+                        "testResult": {"test_result": "hoge"},
+                        "ResponseMetadata": {"RequestId": "111"},
+                    }
+                },
+        ),
+        (
+                SuccessResult(operation_name="test", result_data=None, request_id="111"),
+                {"testResponse": {"ResponseMetadata": {"RequestId": "111"}}},
+        ),
+    ],
+)
+def test_result(result, response):
+    assert result.generate_response() == dict2xml(response)
+
+
+def test_error_result():
+    result = ErrorResult(NonExistentQueue(), request_id="111")
+
+    assert result.generate_response() == dict2xml(
+        {
+            "ErrorResponse": {
+                "Error": {
+                    "Type": "Sender",
+                    "Code": "AWS.SimpleQueueService.NonExistentQueue",
+                    "Message": "The specified queue does not exist for this wsdl version.",
+                    "Detail": {},
+                },
+                "ResponseMetadata": {"RequestId": "111"},
+            }
+        }
+    )
